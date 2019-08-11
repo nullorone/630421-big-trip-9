05661b9169aa0b2c9ee4856a1021c9bff515146a
nullorone <sy.v@mail.ru>
2019-08-11 13:18:39 +0300

    Добавь компонент меню

---
+++ src/components/menu.js
@@ -0,0 +1,9 @@
+// Разметка меню
+const getTripMenuMarkup = () => `
+        <nav class="trip-controls__trip-tabs  trip-tabs">
+          <a class="trip-tabs__btn" href="#">Table</a>
+          <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Stats</a>
+        </nav>
+`;
+
+export {getTripMenuMarkup};
