2f414b43880e68d673bc40ff5e8fdf1ff5864c4f
nullorone <sy.v@mail.ru>
2019-08-18 16:47:11 +0300

    Добавь универсальных функций

--- src/utils/util.js
+++ src/utils/util.js
@@ -3,5 +3,32 @@ const renderComponent = (elementContainer, markup, where = `afterend`) => {
   return elementContainer.insertAdjacentHTML(where, markup);
 };
 
-export {renderComponent};
+// Случайное значение от 0 до передаваемого
+const getRandomValueOfProps = (number) => {
+  return Math.floor(Math.random() * number);
+};
+
+// Перемешиваем элементы в массиве
+const getShuffleArray = (array) => {
+  let cloneArray = array.slice();
+  let randomIndex;
+  let tempElement;
+  for (let i = 0; i < cloneArray.length; i++) {
+    randomIndex = getRandomValueOfProps(i + 1);
+    [tempElement, cloneArray[randomIndex]] = [cloneArray[randomIndex], cloneArray[i]];
+    cloneArray[i] = tempElement;
+  }
+  return cloneArray;
+};
+
+// Случайное булево значение
+const getRandomBoolean = () => {
+  return Boolean(Math.round(Math.random()));
+};
+
+export {
+  renderComponent,
+  getShuffleArray,
+  getRandomBoolean
+};
 
