e9aa1c3f02671b572fdb3ebac611aa33a1306f6f
nullorone <sy.v@mail.ru>
2019-08-18 16:48:27 +0300

    Используй моковые данные для заполнения шаблона

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,9 @@
+import {getMockMenu} from "../data";
+
 // Разметка меню
 const getTripMenuMarkup = () => `
         <nav class="trip-controls__trip-tabs  trip-tabs">
-          <a class="trip-tabs__btn" href="#">Table</a>
-          <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Stats</a>
+        ${getMockMenu().map(({title, active}) => `<a class="trip-tabs__btn ${active ? `trip-tabs__btn--active` : ``}" href="#">${title}</a>`).join(``)}
         </nav>
 `;
 
