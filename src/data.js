bd0461e40b3148967626a8ed2cb5dfa17d833d02
nullorone <sy.v@mail.ru>
2019-09-11 22:13:32 +0300

    Исправь id

--- src/data.js
+++ src/data.js
@@ -87,37 +87,37 @@ const types = [
   {
     transfer: [
       {
-        id: `bus-to`,
+        id: `bus`,
         iconSrc: `./img/icons/bus.png`,
         title: `Bus to`,
       },
       {
-        id: `drive-to`,
+        id: `drive`,
         iconSrc: `./img/icons/drive.png`,
         title: `Drive to`,
       },
       {
-        id: `flight-to`,
+        id: `flight`,
         iconSrc: `./img/icons/flight.png`,
         title: `Flight to`,
       },
       {
-        id: `ship-to`,
+        id: `ship`,
         iconSrc: `./img/icons/ship.png`,
         title: `Ship to`,
       },
       {
-        id: `taxi-to`,
+        id: `taxi`,
         iconSrc: `./img/icons/taxi.png`,
         title: `Taxi to`,
       },
       {
-        id: `train-to`,
+        id: `train`,
         iconSrc: `./img/icons/train.png`,
         title: `Train to`,
       },
       {
-        id: `transport-to`,
+        id: `transport`,
         iconSrc: `./img/icons/transport.png`,
         title: `Transport to`,
       }
@@ -129,12 +129,12 @@ const types = [
         title: `Check in`,
       },
       {
-        id: `sightseeing-at`,
+        id: `sightseeing`,
         iconSrc: `./img/icons/sightseeing.png`,
         title: `Sightseeing at`,
       },
       {
-        id: `eat-at`,
+        id: `restaurant`,
         iconSrc: `./img/icons/restaurant.png`,
         title: `Eat at`,
       }
