5456863c0614ab89644fb91c57ac613620a59f6b
nullorone <sy.v@mail.ru>
2019-10-03 20:30:21 +0300

    Добавь сообщение загрузки маршрута

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {menuTitles, apiData} from "./data";
 import TripController from "./controllers/trip-controller";
-import {getSortEventList, renderComponent} from "./utils/util";
+import {createElement, getSortEventList, renderComponent, unrenderComponent} from "./utils/util";
 import Menu from "./components/menu";
 import Stats from "./components/stats";
 import Api from "./api";
@@ -12,6 +12,7 @@ const stats = new Stats();
 const tripEventsContainer = document.querySelector(`.trip-events`);
 const tripControls = document.querySelector(`.trip-controls > h2:first-child`);
 const addEventButton = document.querySelector(`.trip-main__event-add-btn`);
+const startingMessage = `<p class="trip-events__msg">Loading...</p>`;
 
 const tripController = new TripController();
 
@@ -30,6 +31,7 @@ const renderLayout = () => {
   renderComponent(tripControls, menu.getElement());
   renderComponent(tripEventsContainer, stats.getElement(), `afterend`);
   stats.getElement().classList.add(`visually-hidden`);
+  renderComponent(tripEventsContainer, createElement(startingMessage));
 };
 
 renderLayout();
@@ -38,6 +40,7 @@ new Api(apiData)
   .getPoints()
   .then(ModelEvent.parseEvents)
   .then((events) => {
+    unrenderComponent(tripEventsContainer.querySelector(`.trip-events__msg`));
     tripController.init(getSortEvents(events));
   });
 
